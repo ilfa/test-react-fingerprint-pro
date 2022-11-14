e5f29ff490e3af9f1efe9007c6d933567563422f
Ilya Taratukhin <darkilfa@gmail.com>
2022-11-14 19:17:16 +0100

    create fingerprint-pre react demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useVisitorData } from '@fingerprintjs/fingerprintjs-pro-react';
 
 function App() {
+  const {
+    getData,
+    data,
+  } = useVisitorData({extendedResult: true}, { immediate: false });
+
+  const updateData = () => {
+      getData({ignoreCache: true})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="App-identifyButton" onClick={updateData}>Identify</button>
+      <pre className="App-userData">{JSON.stringify(data, null, ' ')}</pre>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FpjsProvider } from '@fingerprintjs/fingerprintjs-pro-react';
 
+const publicApiKey = process.env.REACT_APP_FP_PUBLIC_KEY;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <FpjsProvider
+            loadOptions={{
+                apiKey: publicApiKey
+            }}
+        >
+            <App/>
+        </FpjsProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
