d0b85d5ba9d6b817db2664776e13cea855cec8de
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2022-12-05 21:11:51 +0100

    now this project don't use react integration, supports cloudfront integration and can measure identification time.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,62 @@
 import './App.css';
-import { useVisitorData } from '@fingerprintjs/fingerprintjs-pro-react';
+import FingerprintJS from '@fingerprintjs/fingerprintjs-pro';
+import {useRef, useState} from "react";
+
+const publicApiKey = process.env.REACT_APP_FP_PUBLIC_KEY;
+const behaviorPath = process.env.REACT_APP_FP_BEHAVIOR_PATH;
+const downloadPath = process.env.REACT_APP_FP_DOWNLOAD_PATH;
+const resultPath = process.env.REACT_APP_FP_RESULT_PATH;
+const useCloudfront = process.env.REACT_APP_FP_USE_CLOUDFRONT;
+
+console.log(useCloudfront, !!useCloudfront)
+
+const loadOptions = useCloudfront ? {
+    apiKey: publicApiKey,
+    scriptUrlPattern: `/${behaviorPath}/${downloadPath}?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>`,
+    endpoint: `/${behaviorPath}/${resultPath}`,
+} : {
+    apiKey: publicApiKey,
+};
+
+const wait = (time) => {
+ return new Promise((resolve) => {
+     setTimeout(resolve, time)
+ });
+}
 
 function App() {
-  const {
-    getData,
-    data,
-  } = useVisitorData({extendedResult: true}, { immediate: false });
+  const [data, updateData] = useState();
+  const stat = useRef({});
 
-  const updateData = () => {
-      getData({ignoreCache: true})
+  const doIdentification = async (fp, attempt) => {
+      performance.mark(`identification${attempt}`)
+      const result = await fp.get({extendedResult: true})
+      performance.mark(`identification${attempt}-done`)
+      const identificationDuration = performance.measure('identification', `identification${attempt}`, `identification${attempt}-done`)
+      stat.current[`identification ${attempt} duration:`] = identificationDuration.duration;
+      console.log(`identification ${attempt} duration:`, identificationDuration.duration)
+      updateData(result);
+  }
+
+  const loadData = async () => {
+      performance.mark('initiate-load')
+      const fpPromise = FingerprintJS.load(loadOptions)
+      const fp = await fpPromise
+      performance.mark('agent-loaded')
+      const loadingDuration = performance.measure('agent-loading', 'initiate-load', 'agent-loaded')
+      stat.current['agent-loading'] = loadingDuration.duration
+      console.log('agent loading duration:', loadingDuration.duration)
+      await doIdentification(fp, 1);
+      await wait(10000)
+      await doIdentification(fp, 2);
+      await wait(20000)
+      await doIdentification(fp, 3);
+      console.table(stat.current);
   }
   return (
     <div className="App">
-      <button className="App-identifyButton" onClick={updateData}>Identify</button>
+      <button className="App-identifyButton" onClick={loadData}>Identify</button>
+      <pre className="App-userData">{JSON.stringify(stat, null, ' ')}</pre>
       <pre className="App-userData">{JSON.stringify(data, null, ' ')}</pre>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,19 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { FpjsProvider } from '@fingerprintjs/fingerprintjs-pro-react';
 
-const publicApiKey = process.env.REACT_APP_FP_PUBLIC_KEY;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <FpjsProvider
-            loadOptions={{
-                apiKey: publicApiKey
-            }}
-        >
-            <App/>
-        </FpjsProvider>
+        <App/>
     </React.StrictMode>
 );
 
